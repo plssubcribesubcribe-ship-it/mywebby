a029519d80d0971bfd4c71074e358981b0981b74
plssubcribesubcribe-ship-it <plssubcribesubcribe@gmail.com>
2026-05-01 10:28:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("loaded")
+<div id="app"></div>
+
+<script>
+fetch("https://cdn.jsdelivr.net/gh/plssubcribesubcribe-ship-it/mywebby@main/index.html")
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("app").innerHTML = html;
+  });
+</script>
